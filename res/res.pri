df3be31526b685108358f8608e514e59b0b75881
xavi-b <developer@xavi-b.fr>
2022-01-02 16:09:29 +0100

    Plugins translations

---
+++ res/res.pri
@@ -0,0 +1,7 @@
+SUBDIRS += \
+    translations/
+
+include(translations/translations.pri)
+
+RESOURCES += \
+    $$PWD/karunit_led_display.qrc
---
+++ res/translations/fr_FR.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>widget</name>
+    <message>
+        <source>Refresh</source>
+        <translation>Recharger</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Envoyer</translation>
+    </message>
+</context>
+</TS>
